a8b127b2cab56628ccfa8e6001bbfae8a45e468f
Daniel Mateos <daniel.mat.lab@gmail.com>
2023-07-03 16:40:54 +0200

    Test AppLink

--- src/components/AppLink.test.tsx
+++ src/components/AppLink.test.tsx
@@ -1,10 +1,17 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
 import AppLink from "./AppLink";
 
 describe("AppLink", () => {
-  it("renders", async () => {
-    render(<AppLink />); 
+  it("should render correctly", () => {
+    render(<AppLink href="/test" text="Test" />);
+
+    expect(screen.getByText("Test")).toBeInTheDocument();
+  });
+
+  it("should add additional classes", () => {
+    render(<AppLink href="/test" text="Test" className="test-class" />);
+
+    expect(screen.getByText("Test")).toHaveClass("test-class");
   });
 });
