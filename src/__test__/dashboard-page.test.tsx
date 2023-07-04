ef4a6d605d42b74012abd11c28840506a21f7aea
Daniel Mateos <daniel.mat.lab@gmail.com>
2023-07-04 19:28:15 +0200

    Create Dashboard page

---
+++ src/__test__/dashboard-page.test.tsx
@@ -0,0 +1,60 @@
+import Dashboard from "@/pages/dashboard";
+import { UserWithoutPassword } from "@/types/user";
+import { render } from "@/utils/testUtils";
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+const mockPush = jest.fn();
+jest.mock("next/router", () => ({
+  useRouter: () => ({ push: mockPush }),
+}));
+
+const mockFetch = jest.fn();
+jest.mock("../services/fetch", () => ({
+  useFetch: () => ({ appPostFetch: mockFetch }),
+}));
+
+const mockLogout = jest.fn();
+jest.mock("../services/authentication", () => ({
+  useLogout: () => ({ logout: mockLogout, loading: false }),
+}));
+
+describe("Dashboard Page", () => {
+  it("should show unauthenticated warning page if user is not authenticated", () => {
+    render(<Dashboard />);
+
+    expect(
+      screen.getByRole("heading", {
+        name: "Wait a minute, you are not logged in!",
+      }),
+    ).toBeInTheDocument();
+  });
+
+  it("should show dashboard page if user is authenticated", () => {
+    const mockUser = { id: 1, username: "TestUser" } as UserWithoutPassword;
+    render(<Dashboard />, { initialValues: { user: mockUser } });
+
+    expect(
+      screen.getByRole("heading", { name: "Dashboard" }),
+    ).toBeInTheDocument();
+    expect(screen.getByText(mockUser.username)).toBeInTheDocument();
+  });
+
+  it("should have a logout button", async () => {
+    const mockUser = { id: 1, username: "TestUser" } as UserWithoutPassword;
+    render(<Dashboard />, { initialValues: { user: mockUser } });
+
+    expect(
+      screen.getByRole("button", { name: "Disconnect" }),
+    ).toBeInTheDocument();
+  });
+
+  it("should handle logout correctly", async () => {
+    const mockUser = { id: 1, username: "TestUser" } as UserWithoutPassword;
+    render(<Dashboard />, { initialValues: { user: mockUser } });
+
+    await userEvent.click(screen.getByRole("button", { name: "Disconnect" }));
+
+    expect(mockLogout).toHaveBeenCalledTimes(1);
+  });
+});
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,27 @@
-export default function Dashboard() {
+import Button from "@/components/Button";
+import Heading from "@/components/Heading";
+import ProtectedRoute from "@/components/ProtectedRoute";
+import UserData from "@/components/UserData";
+import { useLogout } from "@/services/authentication";
+import { useUser } from "@/services/user";
+
+const Dashboard = () => {
+  const { user } = useUser();
+  const { logout, loading: loadingLogout } = useLogout();
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
+    <ProtectedRoute>
+      <div className="pt-4">
+        <Heading text="Dashboard" />
+        <div className="mb-4">
+          <UserData user={user} />
+        </div>
+        <Button variant="secondary" disabled={loadingLogout} onClick={logout}>
+          Disconnect
+        </Button>
+      </div>
+    </ProtectedRoute>
   );
-}
+};
+
+export default Dashboard;
